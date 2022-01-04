93e0c3424d6edc391fe0fd9e0fa5c19430f4d882
khurram <kf115462@gmail.com>
2022-01-04 12:08:58 +0500

    Bloc Patetrn Implementation | Fetching List of Movies | Displaying Banners of Movies | using bloc

---
+++ android/app/src/main/kotlin/com/vendians/bloc_exmaple_movies_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vendians.bloc_exmaple_movies_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/models/item_model.dart
@@ -0,0 +1,164 @@
+/// page : 1
+/// results : [{"adult":false,"backdrop_path":"/o76ZDm8PS9791XiuieNB93UZcRV.jpg","genre_ids":[27,28,878],"id":460458,"original_language":"en","original_title":"Resident Evil: Welcome to Raccoon City","overview":"Once the booming home of pharmaceutical giant Umbrella Corporation, Raccoon City is now a dying Midwestern town. The company’s exodus left the city a wasteland…with great evil brewing below the surface. When that evil is unleashed, the townspeople are forever…changed…and a small group of survivors must work together to uncover the truth behind Umbrella and make it through the night.","popularity":9400.802,"poster_path":"/6WR7wLCX0PGLhj51qyvK8MIxtT5.jpg","release_date":"2021-11-24","title":"Resident Evil: Welcome to Raccoon City","video":false,"vote_average":6.1,"vote_count":637},{"adult":false,"backdrop_path":"/1Rr5SrvHxMXHu5RjKpaMba8VTzi.jpg","genre_ids":[28,12,878],"id":634649,"original_language":"en","original_title":"Spider-Man: No Way Home","overview":"Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.","popularity":10314.745,"poster_path":"/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg","release_date":"2021-12-15","title":"Spider-Man: No Way Home","video":false,"vote_average":8.4,"vote_count":3351},{"adult":false,"backdrop_path":"/hv7o3VgfsairBoQFAawgaQ4cR1m.jpg","genre_ids":[28,878],"id":624860,"original_language":"en","original_title":"The Matrix Resurrections","overview":"Plagued by strange memories, Neo's life takes an unexpected turn when he finds himself back inside the Matrix.","popularity":8479.71,"poster_path":"/8c4a8kE7PizaGQQnditMmI1xbRp.jpg","release_date":"2021-12-16","title":"The Matrix Resurrections","video":false,"vote_average":7.1,"vote_count":1576},{"adult":false,"backdrop_path":"/3G1Q5xF40HkUBJXxt2DQgQzKTp5.jpg","genre_ids":[16,35,10751,14],"id":568124,"original_language":"en","original_title":"Encanto","overview":"The tale of an extraordinary family, the Madrigals, who live hidden in the mountains of Colombia, in a magical house, in a vibrant town, in a wondrous, charmed place called an Encanto. The magic of the Encanto has blessed every child in the family with a unique gift from super strength to the power to heal—every child except one, Mirabel. But when she discovers that the magic surrounding the Encanto is in danger, Mirabel decides that she, the only ordinary Madrigal, might just be her exceptional family's last hope.","popularity":8658.391,"poster_path":"/4j0PNHkMr5ax3IA8tjtxcmPU3QT.jpg","release_date":"2021-11-24","title":"Encanto","video":false,"vote_average":7.8,"vote_count":1788},{"adult":false,"backdrop_path":"/vIgyYkXkg6NC2whRbYjBD7eb3Er.jpg","genre_ids":[878,28,12],"id":580489,"original_language":"en","original_title":"Venom: Let There Be Carnage","overview":"After finding a host body in investigative reporter Eddie Brock, the alien symbiote must face a new enemy, Carnage, the alter ego of serial killer Cletus Kasady.","popularity":5753.972,"poster_path":"/rjkmN1dniUHVYAtwuV3Tji7FsDO.jpg","release_date":"2021-09-30","title":"Venom: Let There Be Carnage","video":false,"vote_average":7.2,"vote_count":5471},{"adult":false,"backdrop_path":"/dK12GIdhGP6NPGFssK2Fh265jyr.jpg","genre_ids":[28,35,80,53],"id":512195,"original_language":"en","original_title":"Red Notice","overview":"An Interpol-issued Red Notice is a global alert to hunt and capture the world's most wanted. But when a daring heist brings together the FBI's top profiler and two rival criminals, there's no telling what will happen.","popularity":3572.578,"poster_path":"/lAXONuqg41NwUMuzMiFvicDET9Y.jpg","release_date":"2021-11-04","title":"Red Notice","video":false,"vote_average":6.8,"vote_count":2453},{"adult":false,"backdrop_path":"/cinER0ESG0eJ49kXlExM0MEWGxW.jpg","genre_ids":[28,12,14],"id":566525,"original_language":"en","original_title":"Shang-Chi and the Legend of the Ten Rings","overview":"Shang-Chi must confront the past he thought he left behind when he is drawn into the web of the mysterious Ten Rings organization.","popularity":2844.011,"poster_path":"/1BIoJGKbXjdFDAqUEiA2VHqkK1Z.jpg","release_date":"2021-09-01","title":"Shang-Chi and the Legend of the Ten Rings","video":false,"vote_average":7.8,"vote_count":4843},{"adult":false,"backdrop_path":"/1BqX34aJS5J8PefVnQSfQIEPfkl.jpg","genre_ids":[80,28,53],"id":826749,"original_language":"en","original_title":"Fortress","overview":"The story revolves around a top-secret resort for retired U.S. intelligence officers. A group of criminals led by Balzary breach the compound, hellbent on revenge on Robert, forcing the retired officer and his son to save the day.","popularity":2760.228,"poster_path":"/m76LAg0MchIcIW9i4yXsQPGQJJF.jpg","release_date":"2021-12-17","title":"Fortress","video":false,"vote_average":6.4,"vote_count":69},{"adult":false,"backdrop_path":"/nvxrQQspxmSblCYDtvDAbVFX8Jt.jpg","genre_ids":[18,35,878],"id":646380,"original_language":"en","original_title":"Don't Look Up","overview":"Two low-level astronomers must go on a giant media tour to warn humankind of an approaching comet that will destroy planet Earth.","popularity":2299.899,"poster_path":"/th4E1yqsE8DGpAseLiUrI60Hf8V.jpg","release_date":"2021-12-07","title":"Don't Look Up","video":false,"vote_average":7.3,"vote_count":2566},{"adult":false,"backdrop_path":"/srFi3oLy8tBcpq07xusnAE5XhwE.jpg","genre_ids":[16,35,10751,10402],"id":438695,"original_language":"en","original_title":"Sing 2","overview":"Buster and his new cast now have their sights set on debuting a new show at the Crystal Tower Theater in glamorous Redshore City. But with no connections, he and his singers must sneak into the Crystal Entertainment offices, run by the ruthless wolf mogul Jimmy Crystal, where the gang pitches the ridiculous idea of casting the lion rock legend Clay Calloway in their show. Buster must embark on a quest to find the now-isolated Clay and persuade him to return to the stage.","popularity":2174.088,"poster_path":"/aWeKITRFbbwY8txG5uCj4rMCfSP.jpg","release_date":"2021-12-01","title":"Sing 2","video":false,"vote_average":7.5,"vote_count":119},{"adult":false,"backdrop_path":"/1Wlwnhn5sXUIwlxpJgWszT622PS.jpg","genre_ids":[16,35,10751],"id":585245,"original_language":"en","original_title":"Clifford the Big Red Dog","overview":"As Emily struggles to fit in at home and at school, she discovers a small red puppy who is destined to become her best friend. When Clifford magically undergoes one heck of a growth spurt, becomes a gigantic dog and attracts the attention of a genetics company, Emily and her Uncle Casey have to fight the forces of greed as they go on the run across New York City. Along the way, Clifford affects the lives of everyone around him and teaches Emily and her uncle the true meaning of acceptance and unconditional love.","popularity":1876.079,"poster_path":"/30ULVKdjBcQTsj2aOSThXXZNSxF.jpg","release_date":"2021-11-10","title":"Clifford the Big Red Dog","video":false,"vote_average":7.4,"vote_count":782},{"adult":false,"backdrop_path":"/zlj0zHo67xXoj7hvwGtaKRkSdBV.jpg","genre_ids":[878,53,12],"id":728526,"original_language":"en","original_title":"Encounter","overview":"A decorated Marine goes on a rescue mission to save his two young sons from an unhuman threat. As their journey takes them in increasingly dangerous directions, the boys will need to leave their childhoods behind.","popularity":1735.303,"poster_path":"/tUkY0WxffPZ9PoyC62PIyyUMGnt.jpg","release_date":"2021-12-03","title":"Encounter","video":false,"vote_average":6.3,"vote_count":163},{"adult":false,"backdrop_path":"/lyvszvJJqqI8aqBJ70XzdCNoK0y.jpg","genre_ids":[28,12,14,878],"id":524434,"original_language":"en","original_title":"Eternals","overview":"The Eternals are a team of ancient aliens who have been living on Earth in secret for thousands of years. When an unexpected tragedy forces them out of the shadows, they are forced to reunite against mankind’s most ancient enemy, the Deviants.","popularity":1806.119,"poster_path":"/6AdXwFTRTAzggD2QUTt5B7JFGKL.jpg","release_date":"2021-11-03","title":"Eternals","video":false,"vote_average":7.1,"vote_count":1576},{"adult":false,"backdrop_path":"/mFbS5TwN95BcSEfiztdchLgTQ0v.jpg","genre_ids":[18,36],"id":617653,"original_language":"en","original_title":"The Last Duel","overview":"King Charles VI declares that Knight Jean de Carrouges settle his dispute with his squire, Jacques Le Gris, by challenging him to a duel.","popularity":1611.217,"poster_path":"/zjrJE0fpzPvX8saJXj8VNfcjBoU.jpg","release_date":"2021-10-13","title":"The Last Duel","video":false,"vote_average":7.6,"vote_count":1254},{"adult":false,"backdrop_path":"/gg2w8QYf6o5elN95RHtikQaVIsc.jpg","genre_ids":[28,18,80],"id":592508,"original_language":"hi","original_title":"Sooryavanshi","overview":"A fearless, faithful albeit slightly forgetful Mumbai cop, Veer Sooryavanshi, the chief of the Anti-Terrorism Squad in India pulls out all the stops and stunts to thwart a major conspiracy to attack his city.","popularity":1781.192,"poster_path":"/6daLNbfexDUjOpEvIfVaRsjHXJl.jpg","release_date":"2021-11-05","title":"Sooryavanshi","video":false,"vote_average":5.5,"vote_count":53},{"adult":false,"backdrop_path":"/xGrTm3J0FTafmuQ85vF7ZCw94x6.jpg","genre_ids":[18,36,12],"id":589761,"original_language":"ru","original_title":"Чернобыль","overview":"The aftermath of a shocking explosion at the Chernobyl nuclear power station made hundreds of people sacrifice their lives to clean up the site of the catastrophe and to successfully prevent an even bigger disaster that could have turned a large part of the European continent into an uninhabitable exclusion zone. This is their story.","popularity":1663.927,"poster_path":"/AmJLuHjxPdIJO6vmymeWADG6jK5.jpg","release_date":"2021-04-15","title":"Chernobyl: Abyss","video":false,"vote_average":6.2,"vote_count":250},{"adult":false,"backdrop_path":"/sLWUtbrpiLp23a0XDSiUiltdFPJ.jpg","genre_ids":[28,12,14],"id":1930,"original_language":"en","original_title":"The Amazing Spider-Man","overview":"Peter Parker is an outcast high schooler abandoned by his parents as a boy, leaving him to be raised by his Uncle Ben and Aunt May. Like most teenagers, Peter is trying to figure out who he is and how he got to be the person he is today. As Peter discovers a mysterious briefcase that belonged to his father, he begins a quest to understand his parents' disappearance – leading him directly to Oscorp and the lab of Dr. Curt Connors, his father's former partner. As Spider-Man is set on a collision course with Connors' alter ego, The Lizard, Peter will make life-altering choices to use his powers and shape his destiny to become a hero.","popularity":1874.646,"poster_path":"/fSbqPbqXa7ePo8bcnZYN9AHv6zA.jpg","release_date":"2012-06-23","title":"The Amazing Spider-Man","video":false,"vote_average":6.6,"vote_count":14121},{"adult":false,"backdrop_path":"/weneJTnAb1IFI94SKcaXzBFmPKH.jpg","genre_ids":[80,53,18,28],"id":818192,"original_language":"en","original_title":"Ida Red","overview":"Ida Red may not survive her 20-year prison sentence for armed robbery. She turns to her son, Wyatt, for one last job and a chance to regain her freedom.","popularity":1667.975,"poster_path":"/etMxKseW67499tUJonLNHXTF538.jpg","release_date":"2021-11-05","title":"Ida Red","video":false,"vote_average":6,"vote_count":13},{"adult":false,"backdrop_path":"/tTlAA0REGPXSZPBfWyTW9ipIv1I.jpg","genre_ids":[28,12,878,18],"id":315635,"original_language":"en","original_title":"Spider-Man: Homecoming","overview":"Following the events of Captain America: Civil War, Peter Parker, with the help of his mentor Tony Stark, tries to balance his life as an ordinary high school student in Queens, New York City, with fighting crime as his superhero alter ego Spider-Man as a new threat, the Vulture, emerges.","popularity":1720.442,"poster_path":"/c24sv2weTHPsmDa7jEMN0m2P3RT.jpg","release_date":"2017-07-05","title":"Spider-Man: Homecoming","video":false,"vote_average":7.4,"vote_count":17682},{"adult":false,"backdrop_path":"/mRZDHjArYNWpOv06kxRK1cduQKh.jpg","genre_ids":[27,53],"id":754934,"original_language":"en","original_title":"Son","overview":"When a young boy contracts a mysterious illness, his mother must decide how far she will go to protect him from terrifying forces in her past.","popularity":1346.773,"poster_path":"/4fl6EdtMp6p0RKJgESdFti1J3dC.jpg","release_date":"2021-08-06","title":"Son","video":false,"vote_average":6.4,"vote_count":58}]
+/// total_pages : 31787
+/// total_results : 635736
+
+class ItemModel {
+  ItemModel({
+      int? page, 
+      List<Results>? results, 
+      int? totalPages, 
+      int? totalResults,}){
+    _page = page;
+    _results = results;
+    _totalPages = totalPages;
+    _totalResults = totalResults;
+}
+
+  ItemModel.fromJson(dynamic json) {
+    _page = json['page'];
+    if (json['results'] != null) {
+      _results = [];
+      json['results'].forEach((v) {
+        _results?.add(Results.fromJson(v));
+      });
+    }
+    _totalPages = json['total_pages'];
+    _totalResults = json['total_results'];
+  }
+  int? _page;
+  List<Results>? _results;
+  int? _totalPages;
+  int? _totalResults;
+
+  int? get page => _page;
+  List<Results>? get results => _results;
+  int? get totalPages => _totalPages;
+  int? get totalResults => _totalResults;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['page'] = _page;
+    if (_results != null) {
+      map['results'] = _results?.map((v) => v.toJson()).toList();
+    }
+    map['total_pages'] = _totalPages;
+    map['total_results'] = _totalResults;
+    return map;
+  }
+
+}
+
+/// adult : false
+/// backdrop_path : "/o76ZDm8PS9791XiuieNB93UZcRV.jpg"
+/// genre_ids : [27,28,878]
+/// id : 460458
+/// original_language : "en"
+/// original_title : "Resident Evil: Welcome to Raccoon City"
+/// overview : "Once the booming home of pharmaceutical giant Umbrella Corporation, Raccoon City is now a dying Midwestern town. The company’s exodus left the city a wasteland…with great evil brewing below the surface. When that evil is unleashed, the townspeople are forever…changed…and a small group of survivors must work together to uncover the truth behind Umbrella and make it through the night."
+/// popularity : 9400.802
+/// poster_path : "/6WR7wLCX0PGLhj51qyvK8MIxtT5.jpg"
+/// release_date : "2021-11-24"
+/// title : "Resident Evil: Welcome to Raccoon City"
+/// video : false
+/// vote_average : 6.1
+/// vote_count : 637
+
+class Results {
+  Results({
+      bool? adult, 
+      String? backdropPath, 
+      List<int>? genreIds, 
+      int? id, 
+      String? originalLanguage, 
+      String? originalTitle, 
+      String? overview, 
+      double? popularity, 
+      String? posterPath, 
+      String? releaseDate, 
+      String? title, 
+      bool? video, 
+      double? voteAverage, 
+      int? voteCount,}){
+    _adult = adult;
+    _backdropPath = backdropPath;
+    _genreIds = genreIds;
+    _id = id;
+    _originalLanguage = originalLanguage;
+    _originalTitle = originalTitle;
+    _overview = overview;
+    _popularity = popularity;
+    _posterPath = posterPath;
+    _releaseDate = releaseDate;
+    _title = title;
+    _video = video;
+    _voteAverage = voteAverage;
+    _voteCount = voteCount;
+}
+
+  Results.fromJson(dynamic json) {
+    _adult = json['adult'];
+    _backdropPath = json['backdrop_path'];
+    _genreIds = json['genre_ids'] != null ? json['genre_ids'].cast<int>() : [];
+    _id = json['id'];
+    _originalLanguage = json['original_language'];
+    _originalTitle = json['original_title'];
+    _overview = json['overview'];
+    _popularity = json['popularity'];
+    _posterPath = json['poster_path'];
+    _releaseDate = json['release_date'];
+    _title = json['title'];
+    _video = json['video'];
+    _voteAverage = json['vote_average'];
+    _voteCount = json['vote_count'];
+  }
+  bool? _adult;
+  String? _backdropPath;
+  List<int>? _genreIds;
+  int? _id;
+  String? _originalLanguage;
+  String? _originalTitle;
+  String? _overview;
+  double? _popularity;
+  String? _posterPath;
+  String? _releaseDate;
+  String? _title;
+  bool? _video;
+  dynamic ? _voteAverage;
+  int? _voteCount;
+
+  bool? get adult => _adult;
+  String? get backdropPath => _backdropPath;
+  List<int>? get genreIds => _genreIds;
+  int? get id => _id;
+  String? get originalLanguage => _originalLanguage;
+  String? get originalTitle => _originalTitle;
+  String? get overview => _overview;
+  double? get popularity => _popularity;
+  String? get posterPath => _posterPath;
+  String? get releaseDate => _releaseDate;
+  String? get title => _title;
+  bool? get video => _video;
+  double? get voteAverage => _voteAverage;
+  int? get voteCount => _voteCount;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['adult'] = _adult;
+    map['backdrop_path'] = _backdropPath;
+    map['genre_ids'] = _genreIds;
+    map['id'] = _id;
+    map['original_language'] = _originalLanguage;
+    map['original_title'] = _originalTitle;
+    map['overview'] = _overview;
+    map['popularity'] = _popularity;
+    map['poster_path'] = _posterPath;
+    map['release_date'] = _releaseDate;
+    map['title'] = _title;
+    map['video'] = _video;
+    map['vote_average'] = _voteAverage;
+    map['vote_count'] = _voteCount;
+    return map;
+  }
+
+}
